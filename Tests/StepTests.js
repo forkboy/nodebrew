6a93872a874e2cdb6d73289c2e8ef2cbc2abde19
matthew.bird <matthew.bird@gmail.com>
2016-02-20 00:58:45 +1100

    wip

--- Tests/StepTests.js
+++ Tests/StepTests.js
@@ -1,26 +1,92 @@
 ﻿var assert = require('assert');
 var Steps = require('../lib/steps.js');
 
-describe('Manual Step', function () {
-    it('should be of type Manual', function () {
+describe('Manual Step;', function () {
+    it('Given I am initialised, then I should be of type Manual', function () {
         var step = new Steps.Manual("Test");
         assert.equal(step.name, "Test");
         assert.equal(step.type, "Manual");
     });
 })
 
-describe('Mash Ramp Step', function () {
-    it('should be of type Ramp', function () {
-        var step = new Steps.MashRampStep("Test");
+describe('Temp Ramp Step;', function () {
+    it('Given I am initialised, then I should be of type Ramp', function () {
+        var step = new Steps.TempRamp("Test");
         assert.equal(step.name, "Test");
         assert.equal(step.type, "Ramp");
     });
+
+    it('Given I am initialised and pumpState is not specified, then I should default to pump state true', function () {
+        var step = new Steps.TempRamp("Test");
+
+        assert.equal(step.pumpState, true);
+    });
+
+    it('Given I am initialised and target temp is specified, then I should have the specified target temp', function () {
+        var step = new Steps.TempRamp("Test", 64.5);
+        
+        assert.equal(step.targetTemp, 64.5);
+    });
 })
 
-describe('Mash Hold Step', function () {
-    it('should be of type Hold', function () {
-        var step = new Steps.MashHoldStep("Test");
+describe('Temp Hold Step;', function () {
+    it('Given I am initialised, then I should be of type Hold', function () {
+        var step = new Steps.TempHold("Test");
         assert.equal(step.name, "Test");
         assert.equal(step.type, "Hold");
+    });
+
+    it('Given I am initialised and pumpState is not specified, then I should default to pump state true', function () {
+        var step = new Steps.TempHold("Test");
+        
+        assert.equal(step.pumpState, true);
+    });
+    
+    it('Given I am initialised and duration is specified, then I should have the specified duration', function () {
+        var step = new Steps.TempHold("Test", 30.1);
+        
+        assert.equal(step.duration, 30.1);
+    });
+})
+
+describe('Boil Step;', function () {
+    it('Given I am initialised, then I should be of type Boil', function () {
+        var step = new Steps.Boil("Test");
+        assert.equal(step.name, "Test");
+        assert.equal(step.type, "Boil");
+    });
+    
+    it('Given I am initialised and duration is specified, then I should have the specified duration', function () {
+        var step = new Steps.Boil("Test", 30.1);
+        
+        assert.equal(step.duration, 30.1);
+    });
+})
+
+describe('Chill Step;', function () {
+    it('Given I am initialised, then I should be of type Chill', function () {
+        var step = new Steps.Chill("Test");
+        assert.equal(step.name, "Test");
+        assert.equal(step.type, "Chill");
+    });
+    
+    it('Given I am initialised and duration is specified, then I should have the specified duration', function () {
+        var step = new Steps.Chill("Test", 30.1);
+        
+        assert.equal(step.duration, 30.1);
+    });
+})
+
+describe('Settle Step;', function () {
+    it('Given I am initialised, then I should be of type Settle', function () {
+        var step = new Steps.Settle("Test");
+        assert.equal(step.name, "Test");
+        assert.equal(step.type, "Settle");
+    });
+    
+    it('Given I am initialised and duration is specified, then I should have the specified duration', function () {
+        var step = new Steps.Settle("Test", 30.1);
+        
+        assert.equal(step.duration, 30.1);
     });
 })
--- Tests/WorkflowTests.js
+++ Tests/WorkflowTests.js
@@ -1,20 +1,22 @@
-﻿var assert = require('assert');
-var Events = require('../lib/events.js');
-var Workflow = require('../lib/workflow.js');
+﻿var assert   = require('assert'),
+    Events   = require('../lib/events.js'),
+    Workflow = require('../lib/workflow.js'),
+    Time = require("../lib/time.js"),
+    sinon = require('sinon');
 
 
-describe('Workflow;', function () {
-
+describe('Workflow Steps;', function () {
+    
     it('Given an uninitialised workflow, whem I initialise, then I should be at Start', function () {
         var w = new Workflow.Workflow();
         assert.equal(w.getCurrentStep().name, "Start");
         assert.equal(w.getCurrentStep().type, "Manual");
     });
-
+    
     it('Given I am at the start, when I move next, then I should be on Add Mash Water step', function () {
         var w = new Workflow.Workflow();
         w.moveNext();
-
+        
         assert.equal(w.getCurrentStep().name, "Add Mash Water");
     });
     
@@ -31,7 +33,7 @@ describe('Workflow;', function () {
         
         assert.equal(w.getCurrentStep().name, "Finish");
     });
-
+    
     it('Given I am at the last step, when I move next, then I should remain on the last step', function () {
         var w = new Workflow.Workflow();
         w.goToStep("Finish");
@@ -39,11 +41,11 @@ describe('Workflow;', function () {
         
         assert.equal(w.getCurrentStep().name, "Finish");
     });
-
+    
     it('Given I am at a manual step, when I raise a confirmation event, I should move to the next step', function () {
         var w = new Workflow.Workflow();
         w.goToStep("Start");
-
+        
         w.event(new Events.InputEvent());
         assert.equal(w.getCurrentStep().name, "Add Mash Water");
     });
@@ -54,12 +56,12 @@ describe('Workflow;', function () {
         var e = new Events.TemperatureReachedEvent();
         w.event(e);
         assert.equal(w.getCurrentStep().name, "Start");
-
+        
         var e = new Events.TimeExpiredEvent();
         w.event(e);
         assert.equal(w.getCurrentStep().name, "Start");
     });
-
+    
     it('Given I am at a mash ramp step, when I reach setpoint, I should move to the next step', function () {
         var w = new Workflow.Workflow();
         w.goToStep("Ramp to Strike");
@@ -68,7 +70,7 @@ describe('Workflow;', function () {
         w.event(e);
         assert.equal(w.getCurrentStep().name, "Add Grains");
     });
-
+    
     it('Given I am at a mash ramp step, when I receive a non temperature reached Event, I should do nothing', function () {
         var w = new Workflow.Workflow();
         w.goToStep("Ramp to Strike");
@@ -76,12 +78,12 @@ describe('Workflow;', function () {
         var e = new Events.InputEvent();
         w.event(e);
         assert.equal(w.getCurrentStep().name, "Ramp to Strike");
-
+        
         var e = new Events.TimeExpiredEvent();
         w.event(e);
         assert.equal(w.getCurrentStep().name, "Ramp to Strike");
     });
-
+    
     it('Given I am at a mash wait step, when I receive a Time Expired, I should move to the next step', function () {
         var w = new Workflow.Workflow();
         w.goToStep("Protein Rest");
@@ -90,7 +92,7 @@ describe('Workflow;', function () {
         w.event(e);
         assert.equal(w.getCurrentStep().name, "Ramp to Sacc 1");
     });
-
+    
     it('Given I am at a mash wait step, when I receive a non time-expired event, I should do nothing', function () {
         var w = new Workflow.Workflow();
         w.goToStep("Protein Rest");
@@ -98,10 +100,63 @@ describe('Workflow;', function () {
         var e = new Events.InputEvent();
         w.event(e);
         assert.equal(w.getCurrentStep().name, "Protein Rest");
-
+        
         var e = new Events.TemperatureReachedEvent(30);
         w.event(e);
         assert.equal(w.getCurrentStep().name, "Protein Rest");
     });
-})
+    
+    it('Given I am at a mash wait step, when I receive a non time-expired event, I should do nothing', function () {
+        var w = new Workflow.Workflow();
+        w.goToStep("Protein Rest");
+        
+        var e = new Events.InputEvent();
+        w.event(e);
+        assert.equal(w.getCurrentStep().name, "Protein Rest");
+        
+        var e = new Events.TemperatureReachedEvent(30);
+        w.event(e);
+        assert.equal(w.getCurrentStep().name, "Protein Rest");
+    });
+});
 
+
+describe('Workflow Timer;', function () {
+    
+    it('Given I am at a any step, when I go to a step, the step start time should be recorded', function () {
+        var w = new Workflow.Workflow();
+        sinon.stub(w.time, "MachineSeconds", function () { return 100; });
+        
+        w.goToStep("Protein Rest"); 
+
+        assert.equal(w.stepStartedAt, 100);
+    });
+
+    it('Given I am at a Waitable Step, when the time since start exceeds wait time, I should move to next step', function () {
+        var w = new Workflow.Workflow();
+        
+        var stub = sinon.stub(w.time, "MachineSeconds", function () { return 0 });
+
+        w.goToStep("Protein Rest"); 
+        var stepDuration = w.getCurrentStep().duration * 60;
+        
+        stub.restore();
+        stub = sinon.stub(w.time, "MachineSeconds", function () { return stepDuration - 1 });
+
+        assert.equal(w.checkForTimeExpired(), false, "should be false when elapsed time is < duration");
+
+        stub.restore();
+        stub = sinon.stub(w.time, "MachineSeconds", function () { return stepDuration });
+        
+        console.log(w.time.MachineSeconds());
+
+        assert.equal(w.checkForTimeExpired(), false, "should be false when elapsed time equals duration");
+
+        stub.restore();
+        stub = sinon.stub(w.time, "MachineSeconds", function () { return stepDuration + 1 });
+        
+        console.log(w.time.MachineSeconds());
+        
+        assert.equal(w.checkForTimeExpired(), true, "should be true when elapsed time > duration");
+    });
+});
